42a8d8c5c7a4907ac0927eb3419d9885b325b88c
Sania117 <saniapinnock116@gmail.com>
2020-03-05 22:55:54 +0000

    Make an array of odjects

---
+++ js/script.js
@@ -0,0 +1,16 @@
+ $(".button").click(function(){
+    let input1 = $("#songName").val();
+
+    let input2 = $("#artistName").val();
+
+    let input3 = $("#songLength").val();
+
+    let input4 = $("#imageURL").val();
+
+    let input5 = $("#songLink").val();
+
+     console.log(input1);
+ });
+
+ 
+ 
